4c8f134824c04e2c77ce8b545f7e74767320c37c
roundtablehero <4rghplay@gmail.com>
2025-12-09 18:21:15 +0300

    Add Exception Tests into HorseTest.java Fix statement lambda Warnings

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,8 +1,35 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
+
+    @Test
+    void shouldThrowWhenNameIsNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10.0, 100.0));
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "\n", "\t\n"})
+    void shouldThrowWhenNameIsBlank(String input) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(input, 10.0, 100.0));
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenDistanceIsNegative() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse("TestName", 10.0, -Double.MAX_VALUE));
+        assertEquals("Distance cannot be negative.", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenSpeedIsNegative() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse("TestName", -Double.MAX_VALUE, 100.0));
+        assertEquals("Speed cannot be negative.", exception.getMessage());
+    }
 
     @Test
     void getName() {
